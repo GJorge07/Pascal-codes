154820974de7b8c86b1963ce73ab9db4f570045b
Gabriel Dobrowolski Jorge <gdj25@inf.ufpr.br>
2025-06-20 21:02:02 -0300

    Update matriz4.pas

--- Matriz/matriz4.pas
+++ Matriz/matriz4.pas
@@ -2,39 +2,59 @@ program permutacaoemmatriz;
 
 type matriz = array[1..100,1..100] of longint;
 
-var w:matriz;
+var m:matriz;
     n:longint;
 
-procedure lematriz(var w:matriz;n:longint);
+procedure lematriz(var m:matriz;n:longint);
 var i,j:longint;
 begin
     for i:=1 to n do   
         for j:=1 to n do
-            read (w[i,j]);
+            read (m[i,j]);
 end;
 
-function provareal();
-var 
+procedure permuta(var m:matriz; n:longint);
+var
+    i, j, cont: longint;
+    ok: boolean;
 begin
-    for i:=1
-end;
-
-procedure permuta();
-var i,j:longint;
-begin
-    for i:=1 to n do
-    begin
-        for j:=1 to n do 
+    ok := true;
+    for i := 1 to n do
+    begin {tem esse begin pra zerar o contador em cada linha}
+        cont := 0;
+        for j := 1 to n do
         begin
-            if w[i,j] = 1 then
-                achou:=w[i,j];
+            if (m[i,j] <> 0) and (m[i,j] <> 1) then
+                ok := false;
+            if m[i,j] = 1 then
+                cont := cont + 1;
         end;
-        if achou then provareal();
+        if cont <> 1 then
+            ok := false;
+    end;
+
+    for j := 1 to n do
+    begin
+        cont := 0;
+        for i := 1 to n do
+            if m[i,j] = 1 then
+                cont := cont + 1;
+        if cont <> 1 then
+            ok := false;
+    end;
+    if ok then
+        writeln('sim')
+    else
+        writeln('nao');
 end;
-
-
-
 
 begin
     read(n);
-    lematriz(w,n);
+    lematriz(m,n);
+    permuta(m,n);
+end.
+
+    {Uma matriz inteira A n×n é considerada uma matriz de permutação se em cada linha e em cada coluna
+houver n - 1 elementos nulos e um único elemento igual a 1. Faça um programa em Free Pascal que leia um inteiro positivo n, sendo 1 ≤ m, n ≤ 100, e uma matriz
+inteira A n×n . O programa deve imprimir “sim” caso a matriz A seja de permutação, caso contrário deve
+imprimir “nao”.}
