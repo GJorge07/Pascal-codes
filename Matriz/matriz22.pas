34ac8e8d4c1075608371995142f347f507439bcd
Gabriel Dobrowolski Jorge <gdj25@inf.ufpr.br>
2025-06-27 21:42:54 -0300

    Update matriz22.pas

--- Matriz/matriz22.pas
+++ Matriz/matriz22.pas
@@ -3,17 +3,53 @@ program palavrascruzadas;
 type matriz = array[1..1000,1..1000] of longint;
 
 var
-    m:matriz;
+    w:matriz;
+    m,n:longint;
 
-procedure lematriz();
+
+procedure lematriz(var w:matriz;m,n:longint);
 var i,j:longint;
 begin
-
+    for i:=1 to m do
+        for j:=1 to n do
+            read(w[i,j]);
 end;
 
+procedure escrevematriz(var w:matriz;m,n:longint);
+var i,j:longint;
+begin
+    for i:=1 to m do
+    begin
+        for j:=1 to n do
+            write(w[i,j],' ');
+        writeln;
+    end;
+end;
 
-
+procedure iniciodepalavra(var w:matriz; m, n: longint);
+var i, j, cont: longint;
+begin
+    cont := 0;
+    for i := 1 to m do
+        for j := 1 to n do
+            if w[i, j] = 0 then
+            begin
+                if ((j = 1) or (w[i, j-1] = -1)) and (j < n) and (w[i, j+1] = 0) then
+                begin
+                    cont := cont + 1;
+                    w[i, j] := cont;
+                end
+                else if ((i = 1) or (w[i-1, j] = -1)) and (i < m) and (w[i+1, j] = 0) then
+                begin
+                    cont := cont + 1;
+                    w[i, j] := cont;
+                end;
+            end;
+end;
 
 begin
-
+    read(m,n);
+    lematriz(w,m,n);
+    iniciodepalavra(w,m,n);
+    escrevematriz(w,m,n);
 end.
